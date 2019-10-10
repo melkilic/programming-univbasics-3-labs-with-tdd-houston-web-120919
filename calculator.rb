6197b18bf67ffa390fb5154b315c0f797e8a1739
Melike KILIC <melike180400@gmail.com>
2019-10-10 16:21:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,5 @@
 first_number=3
 second_number=-4
 sum= first_number + second_number
-difference=first_number - second_number
+difference= first_number - second_number
+product= = first_number * second_number
